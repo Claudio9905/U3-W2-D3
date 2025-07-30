5a53f9fd0ba87f56f427558ad9c86fb558e1500a
Claudio9905 <cloudpost10@gmail.com>
2025-07-30 16:57:47 +0200

    update file

--- netflix-clone/src/components/GalleryMovies.jsx
+++ netflix-clone/src/components/GalleryMovies.jsx
@@ -1,18 +1,21 @@
-import { Component } from "react";
+import { Component, useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Spinner from "react-bootstrap/Spinner";
+import { useNavigate } from "react-router-dom";
 
-class GalleryMovies extends Component {
-  state = {
+const GalleryMovies = (props) => {
+  const [state, setState] = useState({
     films: [],
     isLoading: true,
     isError: false,
-  };
+  });
 
-  getMovies = () => {
-    fetch(this.props.endpoint)
+  const navigate = useNavigate();
+
+  const getMovies = () => {
+    fetch(props.endpoint)
       .then((response) => {
         if (response.ok) {
           return response.json();
@@ -22,90 +25,87 @@ class GalleryMovies extends Component {
       })
       .then((movies) => {
         console.log(movies);
-        this.setState({
+        setState({
+          ...state,
           films: movies.Search,
           isLoading: false,
         });
       })
       .catch((err) => {
         console.log("ERRORE: ", err);
-        this.setState({
+        setState({
+          ...state,
           isLoading: false,
           isError: true,
         });
       });
   };
 
-  componentDidMount() {
-    console.log("IO SONO COMPONENTDIDMOUNT");
-    this.getMovies();
-  }
+  useEffect(() => {
+    getMovies();
+  }, []);
 
-  render() {
-    console.log("INVOCATO RENDER");
-    return (
-      <>
-        <Container>
-          <Row className=" flex-nowrap overflow-x-auto g-3 p-3" id="row">
-            {/* spinner per indicare il caricamento delle pagine */}
-            {this.state.isLoading && (
-              <div className="text-center mb-3">
-                <Spinner animation="border" variant="light" />
-              </div>
-            )}
-            {this.state.isError && (
-              <Alert variant="danger" className="text-center">
-                Errore nel caricamento della lista dei film 😥
-              </Alert>
-            )}
+  return (
+    <>
+      <Container>
+        <Row className=" flex-nowrap overflow-x-auto g-3 p-3" id="row">
+          {/* spinner per indicare il caricamento delle pagine */}
+          {state.isLoading && (
+            <div className="text-center mb-3">
+              <Spinner animation="border" variant="light" />
+            </div>
+          )}
+          {state.isError && (
+            <Alert variant="danger" className="text-center">
+              Errore nel caricamento della lista dei film 😥
+            </Alert>
+          )}
 
-            {this.state.films.length === 0 &&
-              !this.state.isLoading &&
-              !this.state.isError && (
-                <Alert variant="info" className="text-center">
-                  Al momento non sono presenti film, coming soon...
-                </Alert>
-              )}
+          {state.films.length === 0 && !state.isLoading && !state.isError && (
+            <Alert variant="info" className="text-center">
+              Al momento non sono presenti film, coming soon...
+            </Alert>
+          )}
 
-            {this.state.films.map((movies) => {
-              return (
-                <Col
-                  className="d-flex justify-content-center "
-                  xs="12"
-                  md="6"
-                  lg="4"
-                  key={movies.imdbID}
+          {state.films.map((movies) => {
+            return (
+              <Col
+                className="d-flex justify-content-center "
+                xs="12"
+                md="6"
+                lg="4"
+                key={movies.imdbID}
+              >
+                <div
+                  className=" d-flex flex-column align-items-center w-100 p-2"
+                  id="box-film"
                 >
-                  <div
-                    className=" d-flex flex-column align-items-center w-100 p-2"
-                    id="box-film"
-                  >
-                    <div className=" d-flex justify-content-center h-75 mt-3">
-                      <img
-                        src={movies.Poster}
-                        alt="Poster del film"
-                        className="img-fluid h-100 rounded-5"
-                        id="img-poster"
-                      />
-                    </div>
-                    <div
-                      className=" mt-5 d-flex flex-column align-items-center "
-                      id="description"
-                    >
-                      <h4 className="text-light text-center ">
-                        {movies.Title}
-                      </h4>
-                      <h4 className=" text-light">{movies.Year}</h4>
-                    </div>
+                  <div className=" d-flex justify-content-center h-75 mt-3">
+                    <img
+                      src={movies.Poster}
+                      alt="Poster del film"
+                      className="img-fluid h-100 rounded-5"
+                      id="img-poster"
+                      onClick={() => {
+                        navigate(`/moviedetails/` + movies.imdbID);
+                      }}
+                    />
                   </div>
-                </Col>
-              );
-            })}
-          </Row>
-        </Container>
-      </>
-    );
-  }
-}
+                  <div
+                    className=" mt-5 d-flex flex-column align-items-center "
+                    id="description"
+                  >
+                    <h4 className="text-light text-center ">{movies.Title}</h4>
+                    <h4 className=" text-light">{movies.Year}</h4>
+                  </div>
+                </div>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </>
+  );
+};
 
 export default GalleryMovies;
--- netflix-clone/src/components/MovieDetails.jsx
+++ netflix-clone/src/components/MovieDetails.jsx
@@ -1,4 +1,10 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+
 const MovieDetails = () => {
+  const params = useParams();
+  console.log("PARAMS", params);
   return <></>;
 };
 
